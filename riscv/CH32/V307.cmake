91623a82af8e6d70142b503e5eebb82e92555172
rernamp <renamp100@gmail.com>
2023-10-08 22:32:54 +0300

    some fix

--- riscv/CH32/V307.cmake
+++ riscv/CH32/V307.cmake
@@ -1,8 +1,8 @@
-set(MCU_ARCH cortex-m7)
-set(MCU_FLOAT_ABI softfp)
-set(MCU_FPU fpv4-sp-d16)
+set(MCU_ARCH rv32imafc )
+set(MCU_FLOAT_ABI ilp32f )
 
-set(COMMON_COMPILE_FLAGS -mabi=ilp32f -mcmodel=medany -msmall-data-limit=8 -mno-save-restore)
+
+set(COMMON_COMPILE_FLAGS -march=${MCU_ARCH}  -mabi=${MCU_FLOAT_ABI} -msmall-data-limit=8 )
 set(C_CXX_COMPILE_FLAGS  -ffunction-sections -fdata-sections -fsigned-char -g -fmessage-length=0)
 
 if(NOT (TARGET CH32::V307))
@@ -13,24 +13,24 @@ if(NOT (TARGET CH32::V307))
         ${COMMON_COMPILE_FLAGS}
     )
 
-    #target_compile_options(CH32::V307 INTERFACE $<$<COMPILE_LANGUAGE:ASM>:-x assembler-with-cpp>)
     target_compile_options(CH32::V307 INTERFACE $<$<COMPILE_LANGUAGE:C>:${C_CXX_COMPILE_FLAGS}>)
     target_compile_options(CH32::V307 INTERFACE $<$<COMPILE_LANGUAGE:CXX>:${C_CXX_COMPILE_FLAGS}>)
     target_compile_options(CH32::V307 INTERFACE $<$<COMPILE_LANGUAGE:CXX>:-fno-exceptions -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics>)
 
-    target_compile_options(CH32::V307 INTERFACE -march=rv32imafc -mabi=ilp32f -mcmodel=medany -msmall-data-limit=8 -mno-save-restore)
-    target_compile_options(CH32::V307 INTERFACE -Xlinker -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -fsingle-precision-constant)
 
     target_compile_options(CH32::V307 INTERFACE $<$<COMPILE_LANGUAGE:ASM>:-x assembler-with-cpp>)
+    
 
     target_link_options(CH32::V307 INTERFACE 
         --sysroot="${TOOLCHAIN_SYSROOT}"
         ${COMMON_COMPILE_FLAGS}
         ${C_CXX_COMPILE_FLAGS} 
         LINKER:-gc-sections 
+        -nostartfiles
     )
 
     target_compile_definitions(CH32::V307 INTERFACE 
+        # STM32F4
         "__weak=__attribute__((weak))"
         "__packed=__attribute__((__packed__))"
     )
